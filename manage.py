2ff30c94de21db9affaf82e5ab3041bfade36708
jadhavpandu <2022bit054@sggs.ac.in>
2025-04-24 10:20:15 +0530

    backend project

--- manage.py
+++ manage.py
@@ -6,7 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
+<<<<<<< HEAD
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chaiheadq.settings')
+=======
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TY_IT_8Sept.settings')
+>>>>>>> 16bf798 (backend project)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
